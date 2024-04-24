da799422b930429a15784413a069b63e8a749cee
Hristo Yonkov <hristopturs@gmail.com>
2024-04-24 21:43:14 +0300

    add some styles to future card component

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,1 +1,10 @@
 // Styled elements for the Card go here
+import styled from "styled-components";
+
+
+export const StyledCard = styled(({ ...props }) => <article {...props} />)`
+  width: 400px;
+  height: 110px;
+  background-color: rgb(237, 237, 237);
+  border-radius: 12px;
+`;
