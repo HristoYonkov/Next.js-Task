88b81db6e9af7a59eafe7e4d5102485ce4a42d84
Hristo Yonkov <hristopturs@gmail.com>
2024-04-24 21:43:30 +0300

    create card component

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,12 @@
 // The Card to be exported goes here
+import { StyledCard } from "./elements";
+
+export const Card = ({ children, ...props }) => {
+  return (
+    <StyledCard {...props}>
+        asdassda
+        asdassdaa
+        asdas
+    </StyledCard>
+  );
+};
