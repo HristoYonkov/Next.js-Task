a758eb67b404aa0d8832e218055763f2c7f3a30b
Hristo Yonkov <hristopturs@gmail.com>
2024-04-25 15:55:29 +0300

    added additional styling for card component

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 
 
 export const StyledCard = styled(({ ...props }) => <article {...props} />)`
-  width: 400px;
+  display: flex;
+  width: 360px;
   height: 110px;
-  background-color: rgb(237, 237, 237);
+  background-color: rgb(249 249 249);
   border-radius: 12px;
+  overflow: hidden;
 `;
